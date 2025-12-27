755768ed18c6ea5818231329020f241409c50d84
Pietro Fiorillo <theorangeturtle.studios@gmail.com>
2025-12-27 19:28:53 +0000

    no se ve

--- src/components/manya/hero-section.tsx
+++ src/components/manya/hero-section.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
-import { ArrowRight, Bot, Linkedin, Search } from 'lucide-react';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 
 export function HeroSection() {
